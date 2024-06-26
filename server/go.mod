9bdfdd0fc04f797b28017444e519099809afb7ab
sambhavKhanna <khsambhav@gmail.com>
2024-06-26 20:49:58 +0300

    successful LLM call

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module rap.ai/server
+
+go 1.22.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ server/index.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"github.com/joho/godotenv"
+)
+
+
+func groq() {
+	godotenv.Load()
+	var GROQ_API_KEY = os.Getenv("GROQ_API_KEY")
+	url := "https://api.groq.com/openai/v1/chat/completions"
+	contentType := "application/json"
+	data := []byte(`{"messages": [{"role": "user", "content": "Explain the importance of fast language models"}], "model": "llama3-8b-8192"}`)
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", contentType)
+	req.Header.Add("Authorization", "Bearer "+ GROQ_API_KEY)
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	var respStr strings.Builder
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	respStr.Write(content)
+	fmt.Println(respStr.String())
+}
+
+func main() {
+	groq()
+}
